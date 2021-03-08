d5e7cc3c0cf2d9a605e8955c02ca5834dbc182d7
sdi1900031 <sdi1900031@di.uoa.gr>
2021-03-08 20:36:16 +0200

    Deleted Trie.cpp

--- Trie/Trie.hpp
+++ Trie/Trie.hpp
@@ -11,16 +11,52 @@ public:
 	};
 
 	// Constructor - Destructor
-	Trie();
-	~Trie();
+	Trie() {
+		root = new TrieNode('\0');
+	}
+
+	~Trie() {
+		destroy(root);
+	}
 	
 	// Functions
-	void insert(const std::string& word);
-	bool search(const std::string& word);
-	bool starts_with(const std::string& word);
+	void insert(const std::string& word) {
+		TrieNode* current = root;
+		for (char c : word) {
+			if (!current->children[c - 'a'])
+				current->children[c - 'a'] = new TrieNode(c);
+			current = current->children[c - 'a'];
+		}
+		current->isWord = true;
+	}
+
+	bool search(const std::string& word) {
+		TrieNode* n = get_TrieNode(word);
+		return n != nullptr && n->isWord;
+	}
+
+	bool starts_with(const std::string& word) {
+		TrieNode* n = get_TrieNode(word);
+		return n != nullptr;
+	}
 
 private:
 	TrieNode* root;
-	TrieNode* get_TrieNode(const std::string& word);
-	void destroy(TrieNode* to_destroy);
+
+	TrieNode* get_TrieNode(const std::string& word) {
+		TrieNode* current = root;
+		for (char c : word) {
+			if (!current->children[c - 'a'])
+				return nullptr;
+			current = current->children[c - 'a'];
+		}
+		return current;		
+	}
+
+	void destroy(TrieNode* node) {
+		for (TrieNode* n : node->children)
+			if (n) 
+				destroy(n);
+		delete node;
+	}
 };
