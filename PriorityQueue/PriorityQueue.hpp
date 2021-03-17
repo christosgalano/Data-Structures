e1f0958668b84947c8243a5c212b2a6eb5c35a52
sdi1900031 <sdi1900031@di.uoa.gr>
2021-03-17 19:24:32 +0200

    Creation

---
+++ PriorityQueue/PriorityQueue.hpp
@@ -0,0 +1,100 @@
+#include "Vector.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <stdexcept>
+#include <utility>
+
+
+// C is set to std::less so the default version is a MaxPriorityQueue,
+// for MinPriorityQueue set the template argument at std::greater upon creation (other
+// custom types can be used, provided that the neccessary function is passed as an argument to the constructor).
+
+template <typename T, typename C = std::less<>>  
+class PriorityQueue {
+private:
+    Vector<T> data;
+    C comparator;
+    std::size_t sz; // Current size of the PQ - not taking into consideration the dummy element of the Vector
+
+    // heapify_up / heapify_down restore the heap property after an insertion / removal
+    void heapify_up(std::size_t index) {
+        if (index == 1)     // We reached the root
+            return;
+
+        int father = index / 2;
+        if (comparator(data[father], data[index])) {
+            std::swap(data[index], data[father]);
+            heapify_up(father);
+        }
+    }
+
+    void heapify_down(std::size_t index) {
+        int left_child = 2 * index;
+        int right_child = left_child + 1;
+
+        if (left_child > sz)  // left_child is a leaf
+            return;
+
+        int max = left_child;
+        if (right_child <= sz && comparator(data[max], data[right_child]))
+            max = right_child;
+
+        if (comparator(data[index], data[max])) {
+            std::swap(data[max], data[index]);
+            heapify_down(max);
+        }
+    }
+
+    // Restores the heap property in O(n)
+    void efficient_heapify() {
+        data.insert(data.begin(), T{});
+        int node = sz / 2;
+        while (node) {
+            heapify_down(node);
+            --node;
+        }
+    }
+
+public:
+    // Constructors - we always use a dummy element at position 0 of the vector so the indexes arithmetic is simpler
+    PriorityQueue() 
+        : data(1, T{}), sz{} {}
+
+    PriorityQueue(C in_comparator)
+        : data(1, T{}), comparator{in_comparator}, sz{} {}
+    
+    PriorityQueue(Vector<T>& vec)
+        : data{vec}, sz{vec.size()}
+    {   
+        efficient_heapify();
+    }
+
+    PriorityQueue(Vector<T>& vec, C in_comparator)
+        : data{vec}, comparator{in_comparator}, sz{vec.size()}
+    {
+        efficient_heapify();
+    }
+
+    void insert(T value) {
+        data.push_back(value);
+        heapify_up(++sz);
+    }
+
+    T& max() {
+        return data[1];
+    }
+    
+    void remove() {
+        if (!sz) return;
+
+        std::swap(data[1], data[sz--]);
+        data.pop_back();
+
+        heapify_down(1);
+    }
+
+    std::size_t size() const { return sz;      }
+    bool empty()       const { return sz == 0; }
+};
---
+++ PriorityQueue/Vector.hpp
@@ -0,0 +1,318 @@
+#include <utility>
+#include <algorithm>
+#include <stdexcept>
+#include <initializer_list>
+
+template <typename T>
+class Vector {
+private:
+    T* data;
+    std::size_t sz;
+    std::size_t cap;
+    static constexpr short min_cap {10};
+    static constexpr short capacity_factor {2};
+public:
+    class Iterator; // Forward declaration
+
+    // Constructors - Destructor
+    Vector() : sz{}, cap{min_cap}
+    {
+        data = new T[cap];
+    }
+
+    explicit Vector(std::size_t size)
+        : sz{size}, cap{ sz < min_cap ? min_cap : sz }
+    {
+        data = new T[cap];
+    }
+
+    Vector(std::size_t size, const T& value)
+        : sz{size}, cap{ sz < min_cap ? min_cap : sz }
+    {
+        data = new T[cap];
+        std::fill(data, data + sz, value);
+    }
+
+    Vector(const std::initializer_list<T>& values)
+        : sz{values.size()}, cap{ sz < min_cap ? min_cap : sz }
+    {
+        data = new T[cap];
+        std::size_t i {};
+        for (auto v : values)
+            data[i++] = v;
+    }
+
+    Vector(const Vector<T>& vec) 
+        : sz{vec.sz}, cap{vec.cap}
+    {
+        data = new T[cap];
+        std::copy(vec.data, vec.data + sz, data);
+    }
+
+    Vector(Vector<T>&& vec)
+        : data{}, sz{}, cap{min_cap}
+    {
+        vec.swap(*this);
+    }
+
+    ~Vector() { delete[] data; }
+
+    // Methods
+    void push_back(const T& value);
+    void push_back(T&& value);
+    void pop_back();
+
+    Iterator erase(Iterator pos);
+    Iterator insert(Iterator pos, const T& value);
+    Iterator find(const T& key);
+
+    T& front();
+    T& back();
+    T& at(std::size_t index);
+
+    std::size_t size()      const  { return sz;  }
+    std::size_t capacity()  const  { return cap; }
+
+    bool empty() const  { return sz == 0; }
+    void clear();
+
+    void swap(Vector<T>& vec);
+
+    // Overloads
+    T&          operator[] (std::size_t index);
+    const T&    operator[] (std::size_t index) const;
+    Vector<T>&  operator=  (const Vector<T>& rhs);
+    Vector<T>&  operator=  (Vector<T>&& rhs) noexcept;
+
+    // Iterator methods
+    Iterator begin() { return Iterator{data};      }
+    Iterator end()   { return Iterator{data + sz}; }
+
+};
+
+template <typename T>
+void Vector<T>::push_back(const T& value) {
+    if (sz == cap) {
+        cap *= capacity_factor;
+
+        T* old = data;
+        data = new T[cap];
+        std::copy(old, old + sz, data);
+
+        delete[] old;
+    }        
+    data[sz++] = value;
+}
+
+template <typename T>
+void Vector<T>::push_back(T&& value) {
+    if (sz == cap) {
+        cap *= capacity_factor;
+
+        T* old = data;
+        data = new T[cap];
+        std::copy(old, old + sz, data);
+
+        delete[] old;
+    }        
+    data[sz++] = value;
+}
+
+template <typename T>
+void Vector<T>::pop_back() {
+    if (sz > 0)
+        --sz;
+}
+
+template<typename T>
+typename Vector<T>::Iterator Vector<T>::erase(Vector<T>::Iterator pos) {
+    std::size_t i {};
+
+    for (auto iter = this->begin(); iter != pos; ++iter, ++i);
+    
+    std::size_t index {i};
+
+    for (auto iter = pos + 1; iter != this->end(); ++iter, ++i) 
+        data[i] = data[i + 1];
+
+    if (index >= --sz)
+        index = 0;
+
+    return Iterator{data + index};
+}
+
+template<typename T>
+typename Vector<T>::Iterator Vector<T>::insert(Vector<T>::Iterator pos, const T& value){
+    std::size_t i = 0;
+    if (cap == sz) {
+        cap *= capacity_factor;
+        
+        T* temp = new T[cap];
+        for (Vector<T>::Iterator iter = this->begin(); iter != pos; ++iter, ++i)
+            temp[i] = data[i];
+        temp[i] = value;
+
+        std::size_t index {i++};
+        
+        for (std::size_t j = 0; j < sz - index; ++j, ++i)
+            temp[i] = data[i - 1];
+
+        delete[] data;
+        
+        data = temp;
+        ++sz;
+
+        return Iterator{data + index};
+    }
+    
+    for(Vector<T>::Iterator iter = this->end(); iter != pos; --iter, ++i)
+        data[sz - i] = data[sz - i - 1];
+    *pos = value;
+    ++sz;
+
+    return pos;
+}
+
+template <typename T>
+T& Vector<T>::front() {
+    if (!sz)
+        throw std::logic_error("vector is empty");
+    return data[0];
+}
+
+template <typename T>
+T& Vector<T>::back() {
+    if (!sz)
+        throw std::logic_error("vector is empty");
+    return data[sz - 1];
+}
+
+template <typename T>
+T& Vector<T>::at(std::size_t index) {
+    if (!sz)
+        throw std::logic_error("vector is empty");
+    else if (index >= sz)
+        throw std::invalid_argument("invalid index");
+    return data[index];
+}
+
+// operator[] overloads do not check that index is not >= sz.
+// If the user wants an exception to be thrown if the index is invalid he should use at(std::size_t).
+template <typename T>
+T& Vector<T>::operator[] (std::size_t index) {
+    return data[index];    
+}
+
+template <typename T>
+const T& Vector<T>::operator[] (std::size_t index) const {
+    return data[index];    
+}
+
+// Because self assignment happens so rarely we don't check that this != &rhs
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& rhs) {
+    Vector<T> temp{rhs};    // Exceptions may occur at this state so we create a temp vector and then swap it with *this
+    temp.swap(*this);    
+    return *this;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(Vector<T>&& rhs) noexcept {
+    rhs.swap(*this);
+    return *this;
+}
+
+template <typename T>
+bool operator==(const Vector<T>& lhs, const Vector<T>& rhs) {
+    if (lhs.size() != rhs.size())
+        return false;
+
+    for (std::size_t i = 0; i < lhs.size(); ++i)
+        if (lhs[i] != rhs[i])
+            return false;
+        
+    return true;   
+}
+
+template <typename T>
+bool operator!=(const Vector<T>& lhs, const Vector<T>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+void Vector<T>::clear() {
+    delete[] data;
+    sz = 0;
+    cap = min_cap;
+    data = new T[cap];
+}
+
+template <typename T>
+void Vector<T>::swap(Vector<T>& vec) {
+    std::swap(sz,   vec.sz);
+    std::swap(cap,  vec.cap);
+    std::swap(data, vec.data);
+}
+
+template <typename T>
+void swap(Vector<T>& lhs, Vector<T>& rhs) {
+    lhs.swap(rhs);
+}
+
+template <typename T>
+typename Vector<T>::Iterator Vector<T>::find(const T& key) {
+    for (auto iter = begin(); iter != end(); ++iter)
+        if (*iter == key)
+            return iter;
+    return end();
+}
+
+template <typename T>
+class Vector<T>::Iterator {
+private:
+    T* data_ptr;
+    friend class Vector;
+public:
+    explicit Iterator() : data_ptr{} {}
+    explicit Iterator(T* in_ptr) : data_ptr{in_ptr} {}
+
+    Iterator(const Iterator&) = default;
+    ~Iterator() = default;
+
+    Iterator& operator++() {
+        ++data_ptr;
+        return *this;
+    }
+
+    Iterator operator++(int) {
+        Iterator temp{*this};
+        ++data_ptr;
+        return temp;
+    }
+
+    Iterator operator+(int d) {
+        data_ptr += d;
+        return *this;
+    }
+
+    Iterator& operator--() {
+        --data_ptr;
+        return *this;
+    }
+
+    Iterator operator--(int) {
+        Iterator temp{*this};
+        --data_ptr;
+        return temp;
+    }
+
+    Iterator operator-(int d) {
+        data_ptr -= d;
+        return *this;
+    }
+
+    bool operator==(const Iterator& rhs) const { return data_ptr == rhs.data_ptr; }
+    bool operator!=(const Iterator& rhs) const { return data_ptr != rhs.data_ptr; }
+
+    T& operator*() const { return *data_ptr; }
+};
