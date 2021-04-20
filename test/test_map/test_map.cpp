bef46ab215006511d60b493e95864f0729018c0e
sdi1900031 <sdi1900031@di.uoa.gr>
2021-04-20 11:10:21 +0300

    Creation

--- test/test_map/test_map.cpp
+++ test/test_map/test_map.cpp
@@ -1,10 +1,42 @@
 #include "gtest/gtest.h"
 #include "Map.hpp"
+#include <string>
 
 using namespace cds;
 
 TEST(Map, constructors) {
-    
+    Map<int, std::string> default_map;
+    EXPECT_EQ(default_map.size(), 0);
+    EXPECT_EQ(default_map.empty(), true);
+
+    Map<int, std::string> map_initializer_list {{1, "Bob"},  {2, "Alice"}};
+    EXPECT_EQ(map_initializer_list.size(), 2);
+    EXPECT_EQ(map_initializer_list.empty(), false);
+    Map<int, std::string>::iterator iter = map_initializer_list.begin();
+    EXPECT_EQ(iter->first, 1);
+    EXPECT_EQ(iter->second, "Bob");
+    ++iter;
+    EXPECT_EQ(iter->first, 2);
+    EXPECT_EQ(iter->second, "Alice");
+
+    Map<int, std::string> copy_map(map_initializer_list);
+    EXPECT_EQ(copy_map.size(), 2);
+    iter = copy_map.begin();
+    EXPECT_EQ(iter->first, 1);
+    EXPECT_EQ(iter->second, "Bob");
+    ++iter;
+    EXPECT_EQ(iter->first, 2);
+    EXPECT_EQ(iter->second, "Alice");
+
+    Map<int, std::string> move_map(std::move(map_initializer_list));
+    EXPECT_EQ(move_map.size(), 2);
+    EXPECT_EQ(map_initializer_list.empty(), true);
+    iter = move_map.begin();
+    EXPECT_EQ(iter->first, 1);
+    EXPECT_EQ(iter->second, "Bob");
+    ++iter;
+    EXPECT_EQ(iter->first, 2);
+    EXPECT_EQ(iter->second, "Alice");
 }
 
 
